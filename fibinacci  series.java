3808d51f8749840f07097c59190f9935e930d324
Laveen pappu <luckysteven5757@gmail.com>
2026-02-25 12:59:45 +0530

    rename

---
+++ fibinacci  series.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        int a=0;
+        int b=1;
+        System.out.print("fibonacci series is:");
+        for(int i=1;i<=n;i++){
+            System.out.print(a+" ");
+            int c=a+b;
+            a=b;
+            b=c;
+            
+        }
+        
+    }
+}
