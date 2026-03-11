ff6f46070ed2d3d1b7c188759224d1786b1e7ee2
Laveen pappu <luckysteven5757@gmail.com>
2026-03-11 22:43:05 +0530

    push

---
+++ count vowels and consonents.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the string");
+        String a=sc.nextLine();
+        int vowels=0;
+        int consonents=0;
+        a=a.toLowerCase();
+        for(int i=0;i<a.length();i++){
+            char ch=a.charAt(i);
+            if(ch>='a'&&ch<='z'){
+                if(ch =='a'||ch =='e'|| ch=='i'||ch=='o'|| ch=='u'){
+                    vowels++;
+                }
+                else{
+                    consonents++;
+                }
+            }
+            
+        }
+        
+        System.out.println("vowels:"+vowels);
+        System.out.println("consonents"+consonents);
+        
+    }
+}
